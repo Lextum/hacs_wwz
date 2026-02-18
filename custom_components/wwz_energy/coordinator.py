f8788f8c88f200faa07e436c3dd6b2a8545ede75
Lextum <5988613+Lextum@users.noreply.github.com>
2026-02-18 22:53:29 +0100

    fix: set mean_type to NONE

--- custom_components/wwz_energy/coordinator.py
+++ custom_components/wwz_energy/coordinator.py
@@ -7,7 +7,7 @@ import logging
 from zoneinfo import ZoneInfo
 
 from homeassistant.components.recorder import get_instance
-from homeassistant.components.recorder.models import StatisticData, StatisticMetaData
+from homeassistant.components.recorder.models import StatisticData, StatisticMeanType, StatisticMetaData
 from homeassistant.components.recorder.statistics import (
     async_add_external_statistics,
     statistics_during_period,
@@ -139,6 +139,7 @@ class WwzEnergyCoordinator(DataUpdateCoordinator[dict]):
 
         metadata = StatisticMetaData(
             has_mean=False,
+            mean_type=StatisticMeanType.NONE,
             has_sum=True,
             name="WWZ Energy",
             source=DOMAIN,
